295a68c502520820af97eacb883034120a678c3f
Onizukapnt <antoine_pont@yahoo.com>
2022-10-20 10:50:27 +0200

    Initial Commit

---
+++ NewProjet/NewProjetApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewProjetApp.swift
+//  NewProjet
+//
+//  Created by Apprenant20 on 20/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct NewProjetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
